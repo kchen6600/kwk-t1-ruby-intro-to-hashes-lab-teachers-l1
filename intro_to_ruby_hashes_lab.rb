0c6980a17b98f8e508acc8630c11b72e8b780ada
Karen Chen <kchen6600@gmail.com>
2018-07-16 16:04:44 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,6 +19,7 @@ def monopoly_with_second_tier
 	monopoly_with_second_tier[:railroads][:pieces] = 4
 	monopoly_with_second_tier[:railroads][:names] = {}
 	monopoly_with_second_tier[:railroads][:rent_in_dollars] = {}
+	return monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
